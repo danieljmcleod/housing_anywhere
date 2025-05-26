6addaa28547c3c37889f54e8afb0da19ecfe0afc
danieljmcleod <d.mcleod@housinganywhere.com>
2025-05-26 16:11:28 +0200

    Update property-listing.tsx

--- components/property-listing.tsx
+++ components/property-listing.tsx
@@ -378,11 +378,10 @@ export function PropertyListing({ id, propertyData }: PropertyListingProps) {
                             <span className="ml-6">Deposit Saver</span>
                               <Info className="h-5 w-5 text-gray-400 ml-2" />
                              {/* Tooltip */}
-                      <div className="absolute left-0 bottom-full mb-2 w-full max-w-md bg-white rounded-lg p-4 shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200 z-10">
+                       className="absolute left-0 bottom-full mb-2 w-full max-w-md bg-white rounded-lg p-4 shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200 z-10">
                         <p className="text-gray-700">
                           This small one-time fee protects the landlord and replaces a traditional deposit. So you pay less upfront and stay in control of your money — that’s a win-win.
                         </p>
-                      </div>
                     </div>
                   )}
                           </div>
